74949bf5feb802174b14004bf098510073f9b35a
Ciaran O'Reilly <ciaran@oreilly.cat>
2021-12-19 16:59:16 +0100

    Integrate pg in searcher

--- searcher/__main__.py
+++ searcher/__main__.py
@@ -1,16 +1,42 @@
+from os import getenv
 from youtubesearchpython import CustomSearch
 
-cs = CustomSearch("ple ordinari", "EgIwAQ%3D%3D", 50)
+import psycopg2
+from psycopg2.extras import Json
+
+PG_HOST = getenv("PG_HOST", "localhost")
+PG_DATABASE = getenv("PG_DATABASE", "datapipe")
+PG_USERNAME = getenv("PG_USERNAME", "datapipe")
+PG_PASSWORD = getenv("PG_PASSWORD")
+
+conn = psycopg2.connect(f"host={PG_HOST} port=5432 dbname={PG_DATABASE} user={PG_USERNAME} password={PG_PASSWORD}")
+
+cur = conn.cursor()
+
+cs = CustomSearch("ple ordinari", "EgIwAQ%3D%3D", 50, 'ca', 'ES')
 
 res = cs.result()
 
-for video in res['result']:
-    print(video['title'])
+counter = 0
 
-cs._next()
+while True:
+    for video in res['result']:
+        link = video['link']
+        print(video['title'])
+        print(f"Checking if source {link} exists in DB")
+        cur.execute(f"SELECT 1 FROM sources WHERE url='{link}'")
+        db_result = cur.fetchone()
+        if db_result:
+            print("Source exists, skipping")
+        else:
+            print("Source does not exist, adding")
+            cur.execute("INSERT INTO sources (url, type, metadata) VALUES (%s,%s, %s)", (link, "youtube", Json(video)))
+    conn.commit()
+    next = cs._next() or False
+    if not next or counter > 50:
+        break
+    res = cs.result()
+    counter += 1
 
-
-res = cs.result()
-
-for video in res['result']:
-    print(video['title'])
+cur.close()
+conn.close()
