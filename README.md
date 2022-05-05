024cae35cda82abf0bc1326827246d44cb677913
Ciaran O'Reilly <ciaran@oreilly.cat>
2022-05-05 15:17:11 +0200

    Make public

---
+++ README.md
@@ -0,0 +1,9 @@
+# Audio datapipe
+
+A data processing pipeline that (currently) extracts audio clips from youtube videos and generates two transcription candidates with a Vosk (Kaldi) and a Wav2Vec2 model. The goal of the software is to ease the generation of datasets for ASR by automatically extracting and processing large audio sources.
+
+This software has been written for internal use and made public as it might be useful for someone. However, no support and no warranty will be provided.
+
+Copyright © 2021 Ciaran O'Reilly
+
+Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE (AGPLv3)
--- k8s/postgresql/tables.sql
+++ k8s/postgresql/tables.sql
@@ -1,63 +1,161 @@
-CREATE TYPE source_type AS ENUM ('youtube', 'tv3', 'ib3', 'tiktok');
-CREATE TYPE source_status AS ENUM ('new', 'downloading', 'downloaded', 'audio_extracting', 'audio_extracted', 'audio_converting', 'audio_converted', 'error');
-CREATE TYPE clip_status AS ENUM ('new', 'splitting', 'split', 'validated');
-CREATE TYPE gender_type AS ENUM ('male', 'female', 'other', 'unknown');
-CREATE TYPE variant_type AS ENUM ('balear', 'central', 'nord-occidental', 'septentrional', 'valencià', 'alguerès', 'unknown');
+-- DROP TYPE public."clip_status";
 
-CREATE TABLE IF NOT EXISTS sources(
-    source_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    url TEXT,
-    type source_type,
-    videopath TEXT,
-    audiopath TEXT,
-    audiopath_16 TEXT,
-    downloaded BOOL DEFAULT FALSE,
-    converted BOOL DEFAULT FALSE,
-    metadata JSONB
+CREATE TYPE public."clip_status" AS ENUM (
+	'new',
+	'split',
+	'validated',
+	'splitting');
+
+-- DROP TYPE public."gender_type";
+
+CREATE TYPE public."gender_type" AS ENUM (
+	'male',
+	'female',
+	'other',
+	'unknown');
+
+-- DROP TYPE public."source_status";
+
+CREATE TYPE public."source_status" AS ENUM (
+	'new',
+	'downloading',
+	'downloaded',
+	'audio_extracting',
+	'audio_extracted',
+	'audio_converting',
+	'audio_converted',
+	'error',
+	'checking_language',
+	'bad_language',
+	'ready_for_download',
+	'vad_running',
+	'vad_done',
+	'splitting',
+	'splitting_done');
+
+-- DROP TYPE public."source_type";
+
+CREATE TYPE public."source_type" AS ENUM (
+	'youtube');
+
+-- DROP TYPE public."variant_type";
+
+CREATE TYPE public."variant_type" AS ENUM (
+	'balear',
+	'central',
+	'nord-occidental',
+	'septentrional',
+	'valencià',
+	'alguerès',
+	'unknown');
+
+-- public.sources definition
+
+-- Drop table
+
+-- DROP TABLE public.sources;
+
+CREATE TABLE public.sources (
+	source_id uuid NOT NULL DEFAULT gen_random_uuid(),
+	url text NULL,
+	"type" public."source_type" NULL,
+	videopath text NULL,
+	audiopath text NULL,
+	audiopath_16 text NULL,
+	metadata jsonb NULL,
+	status public."source_status" NOT NULL DEFAULT 'new'::source_status,
+	duration float4 NULL,
+	sr int4 NULL,
+	license varchar NULL,
+	status_update timestamp NULL,
+	has_captions bool NULL DEFAULT false,
+	CONSTRAINT sources_pkey PRIMARY KEY (source_id)
 );
+CREATE INDEX idx_sources_url ON public.sources USING btree (url);
 
-CREATE TABLE IF NOT EXISTS clips(
-    clip_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    source_id UUID REFERENCES sources(source_id),
-    filepath TEXT,
-    "start" float4 NULL,
+
+-- public.clips definition
+
+-- Drop table
+
+-- DROP TABLE public.clips;
+
+CREATE TABLE public.clips (
+	clip_id uuid NOT NULL DEFAULT gen_random_uuid(),
+	source_id uuid NOT NULL,
+	filepath text NULL,
+	transcript_id uuid NULL,
+	"start" float4 NULL,
 	"end" float4 NULL,
+	"language" varchar NULL,
+	duration float4 NULL,
+	status public."clip_status" NOT NULL DEFAULT 'new'::clip_status,
+	status_update timestamp NULL,
+	CONSTRAINT clips_pkey PRIMARY KEY (clip_id)
 );
 
-CREATE TABLE IF NOT EXISTS transcripts(
-    transcript_id UUID PRIMARY KEY,
-    text TEXT,
-    transcriber CHAR(40),
-    clip_id UUID REFERENCES clips(clip_id)
-);
 
-alter table clips add transcript_id UUID REFERENCES transcripts(transcript_id);
+-- public.genders definition
 
-        
-CREATE INDEX IF NOT EXISTS idx_sources_url ON public.sources USING btree (url);
+-- Drop table
 
-create user grafana with encrypted password 'datapipe';
-grant connect on database datapipe to grafana;
-grant usage on database datapipe to grafana;
-grant usage on schema public to grafana;
-grant select on all tables in schema public to grafana;
-grant select on all sequences in schema public to grafana;
-alter default privileges in schema public grant select on tables to grafana;
+-- DROP TABLE public.genders;
 
-CREATE TABLE IF NOT EXISTS genders (
+CREATE TABLE public.genders (
 	gender_id uuid NOT NULL DEFAULT gen_random_uuid(),
-	gender gender_type NOT NULL DEFAULT 'unknown'::gender_type,
+	gender public."gender_type" NOT NULL DEFAULT 'unknown'::gender_type,
 	origin text NULL,
 	clip_id uuid NULL,
-	CONSTRAINT genders_pkey PRIMARY KEY (gender_id),
-	CONSTRAINT genders_clip_id_fkey FOREIGN KEY (clip_id) REFERENCES clips(clip_id)
+	CONSTRAINT genders_pkey PRIMARY KEY (gender_id)
 );
 
-CREATE TABLE IF NOT EXISTS variants (
+
+-- public.transcripts definition
+
+-- Drop table
+
+-- DROP TABLE public.transcripts;
+
+CREATE TABLE public.transcripts (
+	transcript_id uuid NOT NULL DEFAULT gen_random_uuid(),
+	"text" text NOT NULL,
+	transcriber text NOT NULL,
+	clip_id uuid NOT NULL,
+	CONSTRAINT transcripts_pkey PRIMARY KEY (transcript_id)
+);
+
+
+-- public.variants definition
+
+-- Drop table
+
+-- DROP TABLE public.variants;
+
+CREATE TABLE public.variants (
 	variant_id uuid NOT NULL DEFAULT gen_random_uuid(),
-	variant variant_type NOT NULL DEFAULT 'unknown'::variant_type,
+	variant public."variant_type" NOT NULL DEFAULT 'unknown'::variant_type,
 	origin text NULL,
 	clip_id uuid NULL,
-	CONSTRAINT variants_pkey PRIMARY KEY (variant_id),
-	CONSTRAINT variants_clip_id_fkey FOREIGN KEY (clip_id) REFERENCES clips(clip_id)
+	CONSTRAINT variants_pkey PRIMARY KEY (variant_id)
 );
+
+
+-- public.clips foreign keys
+
+ALTER TABLE public.clips ADD CONSTRAINT clips_source_id_fkey FOREIGN KEY (source_id) REFERENCES public.sources(source_id);
+ALTER TABLE public.clips ADD CONSTRAINT clips_transcript_id_fkey FOREIGN KEY (transcript_id) REFERENCES public.transcripts(transcript_id);
+
+
+-- public.genders foreign keys
+
+ALTER TABLE public.genders ADD CONSTRAINT genders_clip_id_fkey FOREIGN KEY (clip_id) REFERENCES public.clips(clip_id);
+
+
+-- public.transcripts foreign keys
+
+ALTER TABLE public.transcripts ADD CONSTRAINT transcripts_clip_id_fkey FOREIGN KEY (clip_id) REFERENCES public.clips(clip_id);
+
+
+-- public.variants foreign keys
+
+ALTER TABLE public.variants ADD CONSTRAINT variants_clip_id_fkey FOREIGN KEY (clip_id) REFERENCES public.clips(clip_id);
