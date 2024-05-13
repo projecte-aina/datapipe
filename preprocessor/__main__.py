f676e2952adf073259f58cad43c431a716adab62
PaulNdrei <andreipetrea3@gmail.com>
2024-05-13 12:46:39 +0200

    Add onprem docker-compose

--- preprocessor/__main__.py
+++ preprocessor/__main__.py
@@ -10,14 +10,6 @@ from db import get_connection
 from utils import GracefulKiller
 
 from pytube import YouTube
-# from pytube.innertube import _default_clients
-
-# _default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
-# _default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
-# _default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-# _default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-# _default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
-# _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 killer = GracefulKiller()
 
